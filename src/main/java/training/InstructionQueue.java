1292d7eabf3c782042d7f059fc0e593d127b4acc
Lexy Golovina <alexandra.l.golovina@gmail.com>
2017-06-27 23:15:55 +0300

    Refactor  - remove impl folder

--- src/main/java/training/InstructionQueue.java
+++ src/main/java/training/InstructionQueue.java
@@ -1,14 +1,35 @@
 package training;
 
-import training.impl.InstructionMessage;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * Created by Oleksandra_Holovina on 6/27/2017.
  */
-public interface InstructionQueue {
-    void enqueue(InstructionMessage message);
-    InstructionMessage dequeue();
-    InstructionMessage peek();
-    int count();
-    boolean isEmpty();
+public class InstructionQueue {
+    private Queue<InstructionMessage> queue = new PriorityQueue<>(typeComparator);
+
+    public static Comparator<InstructionMessage> typeComparator =
+            Comparator.comparing(InstructionMessage::getInstructionType);
+
+    public void enqueue(InstructionMessage message) {
+        queue.add(message);
+    }
+
+    public InstructionMessage dequeue() {
+        return queue.poll();
+    }
+
+    public InstructionMessage peek() {
+        return queue.peek();
+    }
+
+    public int count() {
+        return queue.size();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
 }
---
+++ src/main/java/training/MessageReceiverImpl.java
@@ -0,0 +1,29 @@
+package training;
+
+import training.*;
+import training.exceptions.ValidationException;
+
+import java.util.List;
+
+/**
+ * Created by Oleksandra_Holovina on 6/27/2017.
+ */
+public class MessageReceiverImpl implements MessageReceiver {
+    private InstructionMessageParser parser = new InstructionMessageParser();
+    private InstructionMessageValidator validator = new InstructionMessageValidator();
+    private InstructionQueue queue = new InstructionQueue();
+
+    @Override
+    public void receive(String text) {
+        List<InstructionMessage> messages = parser.parse(text);
+
+        try {
+            validator.validate(messages);
+            enqueueMultipleMessages(messages);
+        }catch (ValidationException ignored){}
+    }
+
+    private void enqueueMultipleMessages(List<InstructionMessage> messages){
+        messages.forEach(message -> queue.enqueue(message));
+    }
+}
