c7cc0272bd4217fe2e02480c100990128730f962
Oleksandra_Holovina <alexandra.l.golovina@gmail.com>
2017-06-27 14:42:43 +0300

    Create test for message parser

---
+++ src/test/java/training/impl/InstructionMessageParserImplTest.java
@@ -0,0 +1,32 @@
+package training.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import training.InstructionMessageParser;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Oleksandra_Holovina on 6/27/2017.
+ */
+public class InstructionMessageParserImplTest {
+    private InstructionMessageParser parser;
+
+    @Before
+    public void setUp() throws Exception {
+        parser = new InstructionMessageParserImpl();
+    }
+
+    @Test
+    public void splitTextIntoMessages() throws Exception {
+        String text = "Here comes some text with \n new lines";
+        assertEquals(2,parser.splitTextIntoMessages(text).length);
+    }
+
+    @Test
+    public void splitMessageIntoParts() throws Exception {
+        String msg = "Here comes some text";
+        assertEquals(4,parser.splitMessageBySpaces(msg).length);
+    }
+
+}
