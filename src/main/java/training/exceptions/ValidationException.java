225fb04111c952009defa174941d99221b0dedea
Oleksandra_Holovina <alexandra.l.golovina@gmail.com>
2017-06-27 14:54:43 +0300

    Create a custom Validation exception

---
+++ src/main/java/training/exceptions/ValidationException.java
@@ -0,0 +1,10 @@
+package training.exceptions;
+
+/**
+ * Created by Oleksandra_Holovina on 6/26/2017.
+ */
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
