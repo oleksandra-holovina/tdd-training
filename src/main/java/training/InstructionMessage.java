79940215509928c308e9f38532c334829e08a08c
Oleksandra_Holovina <alexandra.l.golovina@gmail.com>
2017-06-30 15:47:33 +0300

    Make message type an enum, add serial number to let FIFO in priority queue

--- src/main/java/training/InstructionMessage.java
+++ src/main/java/training/InstructionMessage.java
@@ -1,20 +1,29 @@
 package training;
 
+import training.enums.MESSAGE_TYPE;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Oleksandra_Holovina on 6/27/2017.
  */
 public class InstructionMessage {
+    private AtomicInteger serialNumGenerator = new AtomicInteger(0);
+    private final int serialNum;
+
     private String instructionMessage;
-    private String instructionType;
+    private MESSAGE_TYPE instructionType;
     private String productCode;
     private int quantity;
     private int UOM;
     private Date timestamp;
 
-    public InstructionMessage(String instructionMessage, String instructionType, String productCode, int quantity, int UOM, Date timestamp) {
+    public InstructionMessage(String instructionMessage, MESSAGE_TYPE instructionType, String productCode, int quantity, int UOM, Date timestamp) {
+        this.serialNum = serialNumGenerator.getAndIncrement();
+
         this.instructionMessage = instructionMessage;
         this.instructionType = instructionType;
         this.productCode = productCode;
@@ -23,11 +32,15 @@ public class InstructionMessage {
         this.timestamp = timestamp;
     }
 
+    public int getSerialNum() {
+        return serialNum;
+    }
+
     public String getInstructionMessage() {
         return instructionMessage;
     }
 
-    public String getInstructionType() {
+    public MESSAGE_TYPE getInstructionType() {
         return instructionType;
     }
 
