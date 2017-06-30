4caa2486734a4a1ce4b523eca6552d721c5e4b97
Oleksandra_Holovina <alexandra.l.golovina@gmail.com>
2017-06-30 15:49:32 +0300

    Add validation to timestamp, refactor code

--- src/main/java/training/InstructionMessageValidator.java
+++ src/main/java/training/InstructionMessageValidator.java
@@ -1,9 +1,11 @@
 package training;
 
+import training.enums.MESSAGE_TYPE;
 import training.exceptions.ValidationException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -11,48 +13,58 @@ import java.util.List;
  * Created by Oleksandra_Holovina on 6/27/2017.
  */
 public class InstructionMessageValidator {
+    private InstructionMessage message;
 
-    public void validate(List<InstructionMessage> messages){
-        messages.forEach(this::validateSingleMessage);
+    public void validate(InstructionMessage message) {
+        this.message = message;
+
+        validateProductCode();
+        validateQuantity();
+        validateUOM();
+        validateTimestamp();
     }
 
-    private void validateSingleMessage(InstructionMessage message){
-        try{
-            validateInstructionType(message);
-        }
-        catch (ValidationException ignored){}
-    }
 
-
-    private void validateInstructionType(InstructionMessage message) {
-        String type = message.getInstructionType();
-        if (!"A".equals(type) && !"B".equals(type) && !"C".equals(type) && !"D".equals(type)){
-            throw new ValidationException("The type "+type+" is not allowed! Available types: A,B,C,D");
-        }
-    }
-
-    private void validateProductCode(InstructionMessage message) {
+    private void validateProductCode() {
         String code = message.getProductCode();
 
-        if(!code.matches("[A-Z]{2}\\d{2}")){
-            throw new ValidationException("The product code "+code+" is not allowed! Available codes: 2 uppercase " +
+        if (code == null || !code.matches("[A-Z]{2}\\d{2}")) {
+            throw new ValidationException("The product code " + code + " is not allowed! Available codes: 2 uppercase " +
                     "letters followed by 2 digits");
         }
     }
 
-    private void validateQuantity(InstructionMessage message) {
+    private void validateQuantity() {
         int quantity = message.getQuantity();
 
-        if (quantity < 0){
+        if (quantity < 0) {
             throw new ValidationException("The quantity should be a number greater than 0");
         }
     }
 
-    private void validateUOM(InstructionMessage message) {
+    private void validateUOM() {
         int uom = message.getUOM();
 
-        if (uom < 0 || uom > 256){
+        if (uom < 0 || uom > 256) {
             throw new ValidationException("The UOM should be in range 0-256");
         }
+    }
+
+    private void validateTimestamp() {
+        Date timestamp = message.getTimestamp();
+        if (timestamp.before(getUnixEpoch()) || timestamp.after(getTodayDate())) {
+            throw new ValidationException("The date should be in range [1 January 1970 - today]");
+        }
+    }
+
+    private Date getUnixEpoch() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1970, Calendar.JANUARY, 1);
+
+        return calendar.getTime();
+    }
+
+    private Date getTodayDate() {
+        return Calendar.getInstance().getTime();
     }
 }
