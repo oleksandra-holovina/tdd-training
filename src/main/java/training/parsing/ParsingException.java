1d7d8fe353167a7fbde085ebcfd3d2dc9f3f111a
Oleksandra_Holovina <alexandra.l.golovina@gmail.com>
2017-07-03 12:13:57 +0300

    Add custom exception for message parser

---
+++ src/main/java/training/parsing/ParsingException.java
@@ -0,0 +1,10 @@
+package training.parsing;
+
+/**
+ * Created by Oleksandra_Holovina on 6/26/2017.
+ */
+public class ParsingException extends RuntimeException {
+    public ParsingException(String message) {
+        super(message);
+    }
+}
