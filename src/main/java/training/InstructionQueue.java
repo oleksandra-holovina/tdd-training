a3cb30d6463bbcd8d7fad00a24ee48bc81d7a82e
Oleksandra_Holovina <alexandra.l.golovina@gmail.com>
2017-07-03 12:17:53 +0300

    Add inner class to wrap an instruction message

--- src/main/java/training/InstructionQueue.java
+++ src/main/java/training/InstructionQueue.java
@@ -1,25 +1,28 @@
 package training;
 
-import java.util.Comparator;
+import training.entities.InstructionMessage;
+import training.entities.MessageType;
+
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Oleksandra_Holovina on 6/27/2017.
  */
 public class InstructionQueue {
-    private Queue<InstructionMessage> queue = new PriorityQueue<>(typeComparator);
+    private Queue<InstructionMessageWrapper> queue = new PriorityQueue<>();
 
     public void enqueue(InstructionMessage message) {
-        queue.add(message);
+        queue.add(wrap(message));
     }
 
     public InstructionMessage dequeue() {
-        return queue.poll();
+        return unwrap(queue.poll());
     }
 
     public InstructionMessage peek() {
-        return queue.peek();
+        return unwrap(queue.peek());
     }
 
     public int count() {
@@ -30,18 +33,60 @@ public class InstructionQueue {
         return queue.isEmpty();
     }
 
-    private static Comparator<InstructionMessage> typeComparator =
-            (message1, message2) -> {
-                int order = findOrderByPriority(message1, message2);
-                return order == 0 ?
-                        message1.getSerialNum() - message2.getSerialNum():
-                        order;
-            };
-
-    private static int findOrderByPriority(InstructionMessage message1, InstructionMessage message2){
-        int priority1 = message1.getInstructionType().getPriority();
-        int priority2 = message2.getInstructionType().getPriority();
-
-        return priority1 - priority2;
+    private InstructionMessageWrapper wrap(InstructionMessage message){
+        return new InstructionMessageWrapper(message);
     }
+
+    private InstructionMessage unwrap(InstructionMessageWrapper wrapper) {
+        if (wrapper == null){
+            return null;
+        }
+        return wrapper.getMessage();
+    }
+
+    private class InstructionMessageWrapper implements Comparable<InstructionMessageWrapper>{
+        private final int serialNum;
+        private AtomicInteger serialNumGenerator = new AtomicInteger(0);
+
+        private InstructionMessage message;
+
+        InstructionMessageWrapper(InstructionMessage message){
+            this.serialNum = generateSerialNumber();
+            this.message = message;
+        }
+
+        public InstructionMessage getMessage() {
+            return message;
+        }
+
+        public int getSerialNum() {
+            return serialNum;
+        }
+
+        @Override
+        public int compareTo(InstructionMessageWrapper messageWrapper) {
+            int priorityOrder = compareByPriority(this, messageWrapper);
+
+            if (priorityOrder == 0){
+                return compareBySerialNumber(this, messageWrapper);
+            }
+            return priorityOrder;
+        }
+
+        private int compareByPriority(InstructionMessageWrapper wrapper1, InstructionMessageWrapper wrapper2){
+            MessageType type1 = wrapper1.getMessage().getInstructionType();
+            MessageType type2 = wrapper2.getMessage().getInstructionType();
+
+            return Integer.compare(type1.getPriority(), type2.getPriority());
+        }
+
+        private int compareBySerialNumber(InstructionMessageWrapper wrapper1, InstructionMessageWrapper wrapper2){
+            return Integer.compare(wrapper1.getSerialNum(), wrapper2.getSerialNum());
+        }
+
+        private int generateSerialNumber(){
+            return serialNumGenerator.getAndIncrement();
+        }
+    }
+
 }
