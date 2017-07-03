fa2e3e79c49c4b4209fc224185aea3e869aa916c
Oleksandra_Holovina <alexandra.l.golovina@gmail.com>
2017-07-03 11:58:00 +0300

    Refactor: rename test methods, change expected exception

--- src/test/java/training/InstructionMessageParserTest.java
+++ src/test/java/training/InstructionMessageParserTest.java
@@ -4,15 +4,18 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import training.InstructionMessageParser;
-import training.exceptions.ValidationException;
-
-import static org.junit.Assert.assertEquals;
+import training.parsing.InstructionMessageParser;
+import training.parsing.ParsingException;
 
 /**
  * Created by Oleksandra_Holovina on 6/27/2017.
  */
 public class InstructionMessageParserTest {
+    private final String INVALID_PARAMS_NUMBER = "Number of arguments should be 6";
+    private final String INVALID_TYPE = "The type is invalid. Available types: A,B,C,D";
+    private final String INVALID_NUMBER = "is not a number";
+    private final String INVALID_TIMESTAMP = "There is an error in timestamp";
+
     private InstructionMessageParser parser;
 
     @Rule
@@ -26,53 +29,53 @@ public class InstructionMessageParserTest {
 
 
     @Test
-    public void checkForInvalidNumberOfArguments() throws Exception {
-        setException("Number of arguments should be 6");
+    public void throwExceptionWhenInvalidNumberOfArguments() throws Exception {
+        setException(INVALID_PARAMS_NUMBER);
 
         String messages = "msg A MZ89 5678 50\n";
         parser.parse(messages);
     }
 
     @Test
-    public void checkForInvalidType() throws Exception {
-        setException("The type is invalid. Available types: A,B,C,D");
+    public void throwExceptionWhenInvalidType() throws Exception {
+        setException(INVALID_TYPE);
 
         String messages = "msg 5 MZ89 5678 50 2015-03-05T10:04:56.012Z\n";
         parser.parse(messages);
     }
 
     @Test
-    public void checkForInvalidQuantity() throws Exception {
-        setException("is not a number");
+    public void throwExceptionWhenInvalidQuantity() throws Exception {
+        setException(INVALID_NUMBER);
 
         String messages = "msg A MZ89 a 50 2015-03-05T10:04:56.012Z\n";
         parser.parse(messages);
     }
 
     @Test
-    public void checkForInvalidUom() throws Exception {
-        setException("is not a number");
+    public void throwExceptionWhenInvalidUom() throws Exception {
+        setException(INVALID_NUMBER);
 
         String messages = "msg A MZ89 5 a 2015-03-05T10:04:56.012Z\n";
         parser.parse(messages);
     }
 
     @Test
-    public void checkForInvalidTimestamp() throws Exception {
-        setException("There is an error in timestamp");
+    public void throwExceptionWhenInvalidTimestamp() throws Exception {
+        setException(INVALID_TIMESTAMP);
 
         String messages = "msg A MZ89 5678 50 2015-03-05\n";
         parser.parse(messages);
     }
 
     @Test
-    public void checkForValidMessage() throws Exception {
+    public void throwExceptionWhenValidMessage() throws Exception {
         String messages = "msg B MZ89 5678 50 2015-03-05T10:04:56.012Z\n";
         parser.parse(messages);
     }
 
     private void setException(String message) {
-        exception.expect(ValidationException.class);
+        exception.expect(ParsingException.class);
         exception.expectMessage(message);
     }
 }
