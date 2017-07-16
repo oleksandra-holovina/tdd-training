b6132260639a09fc11900fb2d549b4eb45ce1a63
Lexy Golovina <alexandra.l.golovina@gmail.com>
2017-07-16 15:38:35 +0300

    Refactor MessageReceiverImplTest: add constants

--- src/test/java/training/MessageReceiverImplTest.java
+++ src/test/java/training/MessageReceiverImplTest.java
@@ -2,35 +2,43 @@ package training;
 
 import org.junit.Before;
 import org.junit.Test;
+import training.instruction_queue.InstructionQueue;
+import training.parsing.InstructionMessageParser;
 import training.parsing.ParsingException;
+import training.validation.InstructionMessageValidator;
 import training.validation.ValidationException;
-import training.interfaces.MessageReceiver;
 
-/**
- * Created by Oleksandra_Holovina on 6/30/2017.
- */
 public class MessageReceiverImplTest {
+    private static final String CORRECT_MESSAGE = "InstructionMessage B MZ89 5678 50 2015-03-05T10:04:56.012Z\n";
+    private static final String MESSAGE_WITH_PARSE_EXCEPTION = "InstructionMessage a MZ89 5678 50\n";
+    private static final String MESSAGE_WITH_VALIDATION_EXCEPTION = "InstructionMessage A M89 5678 50 " +
+            "2015-03-05T10:04:56.012Z\n";
+
     private MessageReceiver receiver;
+    private InstructionMessageParser parser = new InstructionMessageParser();
+    private InstructionMessageValidator validator = new InstructionMessageValidator();
+    private InstructionQueue queue = new InstructionQueue();
 
     @Before
-    public void setUp() throws Exception {
-        receiver = new MessageReceiverImpl();
+    public void setUp() {
+        parser = new InstructionMessageParser();
+        validator = new InstructionMessageValidator();
+        queue = new InstructionQueue();
+
+        receiver = new MessageReceiverImpl(parser, validator, queue);
     }
     @Test
-    public void receiveCorrectMessage() throws Exception {
-        String messages = "msg B MZ89 5678 50 2015-03-05T10:04:56.012Z\n";
-        receiver.receive(messages);
+    public void shouldReceiveCorrectMessage() {
+        receiver.receive(CORRECT_MESSAGE);
     }
 
     @Test(expected = ParsingException.class)
-    public void receiveMessageWithParsingException() throws Exception {
-        String messages = "msg a MZ89 5678 50\n";
-        receiver.receive(messages);
+    public void shouldReceiveMessageWithParsingException() {
+        receiver.receive(MESSAGE_WITH_PARSE_EXCEPTION);
     }
 
     @Test(expected = ValidationException.class)
-    public void receiveMessageWithValidationException() throws Exception {
-        String messages = "msg A M89 5678 50 2015-03-05T10:04:56.012Z\n";
-        receiver.receive(messages);
+    public void shouldReceiveMessageWithValidationException() {
+        receiver.receive(MESSAGE_WITH_VALIDATION_EXCEPTION);
     }
 }
