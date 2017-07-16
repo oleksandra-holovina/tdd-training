ff71b0fc310864f57ce5d8f4a83e634fc9c9d467
Lexy Golovina <alexandra.l.golovina@gmail.com>
2017-07-16 15:43:48 +0300

    Refactor: remove extra comment

--- src/main/java/training/parsing/ParsingException.java
+++ src/main/java/training/parsing/ParsingException.java
@@ -1,8 +1,6 @@
 package training.parsing;
 
-/**
- * Created by Oleksandra_Holovina on 6/26/2017.
- */
+
 public class ParsingException extends RuntimeException {
     public ParsingException(String message) {
         super(message);
--- src/main/java/training/validation/ValidationException.java
+++ src/main/java/training/validation/ValidationException.java
@@ -1,8 +1,5 @@
 package training.validation;
 
-/**
- * Created by Oleksandra_Holovina on 6/26/2017.
- */
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
