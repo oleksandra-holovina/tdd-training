3154acb7dd4a3500a73a82af8cfc07f769d2e3c3
Oleksandra_Holovina <alexandra.l.golovina@gmail.com>
2017-07-18 14:27:43 +0300

    Refactor: separate quantity and uom bounds, move timestamp validation to separate method

--- src/main/java/training/validation/InstructionMessageValidator.java
+++ src/main/java/training/validation/InstructionMessageValidator.java
@@ -8,38 +8,25 @@ import java.time.ZoneOffset;
 
 
 public class InstructionMessageValidator {
-    private static final int LOWER_BOUND = 0;
-    private static final int UPPER_BOUND = 256;
-    private static final String MESSAGE_START = "InstructionMessage";
+    private static final int LOWER_QUANTITY_BOUND = 0;
+    private static final int LOWER_UOM_BOUND = 0;
+    private static final int UPPER_UOM_BOUND = 256;
     private static final String PRODUCT_CODE_REGEX = "[A-Z]{2}\\d{2}";
 
-    private static final String INVALID_MESSAGE_START = "The message should start with InstructionMessage";
     private static final String INVALID_CODE_MESSAGE = "Invalid code. Available codes: 2 uppercase letters followed by 2 digits";
     private static final String INVALID_QUANTITY_MESSAGE = "The quantity should be a number greater than 0";
     private static final String INVALID_UOM_MESSAGE = "The UOM should be in range 0-256";
     private static final String INVALID_TIMESTAMP_MESSAGE = "The date should be in range [1 January 1970 - today]";
 
-    private InstructionMessage message;
-
     public void validate(InstructionMessage message) {
-        this.message = message;
-
-        validateBeginningOfMessage();
-        validateProductCode();
-        validateQuantity();
-        validateUOM();
-        validateTimestamp();
+        validateProductCode(message);
+        validateQuantity(message);
+        validateUOM(message);
+        validateTimestamp(message);
     }
 
-    private void validateBeginningOfMessage(){
-        String messageStart = message.getInstructionMessage();
 
-        if (!messageStart.startsWith(MESSAGE_START)) {
-            throw new ValidationException(INVALID_MESSAGE_START);
-        }
-    }
-
-    private void validateProductCode() {
+    private void validateProductCode(InstructionMessage message) {
         String code = message.getProductCode();
 
         if (code == null || !code.matches(PRODUCT_CODE_REGEX)) {
@@ -47,27 +34,35 @@ public class InstructionMessageValidator {
         }
     }
 
-    private void validateQuantity() {
+    private void validateQuantity(InstructionMessage message) {
         int quantity = message.getQuantity();
 
-        if (quantity < LOWER_BOUND) {
+        if (quantity <= LOWER_QUANTITY_BOUND) {
             throw new ValidationException(INVALID_QUANTITY_MESSAGE);
         }
     }
 
-    private void validateUOM() {
+    private void validateUOM(InstructionMessage message) {
         int uom = message.getUOM();
 
-        if (uom < LOWER_BOUND || uom > UPPER_BOUND) {
+        if (uom < LOWER_UOM_BOUND || uom >= UPPER_UOM_BOUND) {
             throw new ValidationException(INVALID_UOM_MESSAGE);
         }
     }
 
-    private void validateTimestamp() {
+    private void validateTimestamp(InstructionMessage message) {
         LocalDateTime timestamp = message.getTimestamp();
-        if (timestamp.isBefore(getUnixEpoch()) || timestamp.isAfter(getTodayDate())) {
+        if (isTimestampInvalid(timestamp)) {
             throw new ValidationException(INVALID_TIMESTAMP_MESSAGE);
         }
+    }
+
+    private boolean isTimestampInvalid(LocalDateTime timestamp){
+        LocalDateTime unixEpoch = getUnixEpoch();
+
+        return timestamp.isBefore(unixEpoch)
+                || timestamp.equals(unixEpoch)
+                || timestamp.isAfter(getCurrentDateTime());
     }
 
     private LocalDateTime getUnixEpoch() {
@@ -75,7 +70,7 @@ public class InstructionMessageValidator {
         return LocalDateTime.ofInstant(unixEpoch, ZoneOffset.UTC);
     }
 
-    private LocalDateTime getTodayDate() {
+    private LocalDateTime getCurrentDateTime() {
         return LocalDateTime.now();
     }
 }
