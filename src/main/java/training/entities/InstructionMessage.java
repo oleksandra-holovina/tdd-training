7766d37005f12e98962a0ac15f1cc2db00e888ab
Oleksandra_Holovina <alexandra.l.golovina@gmail.com>
2017-07-18 14:34:35 +0300

    Remove extra field

--- src/main/java/training/entities/InstructionMessage.java
+++ src/main/java/training/entities/InstructionMessage.java
@@ -3,24 +3,19 @@ package training.entities;
 import java.time.LocalDateTime;
 
 public class InstructionMessage {
-    private String instructionMessage;
     private MessageType instructionType;
     private String productCode;
     private int quantity;
     private int UOM;
     private LocalDateTime timestamp;
 
-    public InstructionMessage(String instructionMessage, MessageType instructionType, String productCode, int quantity, int UOM, LocalDateTime timestamp) {
-        this.instructionMessage = instructionMessage;
+    public InstructionMessage(MessageType instructionType, String productCode, int quantity, int UOM,
+                              LocalDateTime timestamp) {
         this.instructionType = instructionType;
         this.productCode = productCode;
         this.quantity = quantity;
         this.UOM = UOM;
         this.timestamp = timestamp;
-    }
-
-    public String getInstructionMessage() {
-        return instructionMessage;
     }
 
     public MessageType getInstructionType() {
